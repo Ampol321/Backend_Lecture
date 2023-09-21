0169ac9cce3d295fa1961adf35b31711a2a698ac
Ampol321 <johnpaul.dellova@tup.edu.ph>
2023-09-21 08:59:11 +0800

    AuthController

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
+// use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +19,10 @@ use App\Http\Controllers\PostController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::middleware(['api'])->group(function(){
+    Route::post('/login', [AuthController::class, 'login']);
 });
 
 Route::resource('posts',PostController::class);
