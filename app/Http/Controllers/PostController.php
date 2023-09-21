6374eb01ae11e121a83d863d5ae915a5a32e89c4
Ampol321 <johnpaul.dellova@tup.edu.ph>
2023-09-21 10:14:50 +0800

    Login Authentication

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,6 +8,11 @@ use App\Models\User;
 
 class PostController extends Controller
 {
+
+    public function __construct()
+    {
+        // $this->middleware('auth:api');
+    }
     /**
      * Display a listing of the resource.
      */
